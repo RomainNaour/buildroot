662f11d9d560a3d898b80db676925ba2dd051fb6
Romain Naour <romain.naour@openwide.fr>
2015-04-04 22:04:21 +0200

    package/efl/expedite: fix expedite version to 1.7.10
    In order to bump the efl package, we need to fix expedite
    version since it no longer follows efl-core version.
    
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>

--- package/efl/expedite/expedite.mk
+++ package/efl/expedite/expedite.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-EXPEDITE_VERSION = $(EFL_VERSION)
+EXPEDITE_VERSION = 1.7.10
 EXPEDITE_SITE = http://download.enlightenment.org/releases
 EXPEDITE_LICENSE = BSD-2c
 EXPEDITE_LICENSE_FILES = COPYING
 
-EXPEDITE_DEPENDENCIES = libevas libeina libeet
+EXPEDITE_DEPENDENCIES = efl-core
 
 $(eval $(autotools-package))
