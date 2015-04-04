ef1c99584bcde4bb2d031adb3978ec8efdb21464
Romain Naour <romain.naour@openwide.fr>
2015-04-04 22:11:24 +0200

    package/efl/libelementary: bump to version 1.14.0-beta1
    Update url and add a hash file.
    
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>

--- package/efl/libelementary/libelementary.mk
+++ package/efl/libelementary/libelementary.mk
@@ -4,29 +4,23 @@
 #
 ################################################################################
 
-LIBELEMENTARY_VERSION = $(EFL_VERSION)
-LIBELEMENTARY_SOURCE = elementary-$(LIBELEMENTARY_VERSION).tar.bz2
-LIBELEMENTARY_SITE = http://download.enlightenment.org/releases
+# hardcode the version here since the bump to 1.14.0-beta1 is not complete in
+# Buildroot.
+LIBELEMENTARY_VERSION = 1.14.0-beta1
+LIBELEMENTARY_SOURCE = elementary-$(LIBELEMENTARY_VERSION).tar.xz
+LIBELEMENTARY_SITE = http://download.enlightenment.org/rel/libs/elementary/
 LIBELEMENTARY_LICENSE = LGPLv2.1
 LIBELEMENTARY_LICENSE_FILES = COPYING
 
 LIBELEMENTARY_INSTALL_STAGING = YES
 
-LIBELEMENTARY_DEPENDENCIES = \
-	libeina libevas libecore libedje host-libedje \
-	host-libeet
+LIBELEMENTARY_DEPENDENCIES = host-pkgconf host-efl-core efl-core
 
 LIBELEMENTARY_CONF_OPTS = \
 	--with-edje-cc=$(HOST_DIR)/usr/bin/edje_cc \
-	--with-eet-eet=$(HOST_DIR)/usr/bin/eet
-
-# libethumb_client is only built when ethumbd is built.
-# ethumbd is only built if edbus is built.
-ifeq ($(BR2_PACKAGE_LIBETHUMB)$(BR2_PACKAGE_LIBEDBUS),yy)
-LIBELEMENTARY_DEPENDENCIES += libethumb
-LIBELEMENTARY_CONF_OPTS += --enable-ethumb
-else
-LIBELEMENTARY_CONF_OPTS += --disable-ethumb
-endif
+	--with-eet-eet=$(HOST_DIR)/usr/bin/eet \
+	--with-eolian-gen=$(HOST_DIR)/usr/bin/eolian_gen \
+	--with-doxygen=no \
+	--disable-elementary-test
 
 $(eval $(autotools-package))
