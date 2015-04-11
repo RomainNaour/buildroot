d9aca9a4f6d6e9459fd960945f8a1d05c482371b
Romain Naour <romain.naour@openwide.fr>
2015-04-11 19:14:47 +0200

    package/efl: new host-package
    This new host-package provide edje_cc, embryo_cc and eet binaries
    that will be used by efl packages which will be updated later.
    
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>

---
+++ package/efl/efl.mk
@@ -0,0 +1,74 @@
+################################################################################
+#
+# efl
+#
+################################################################################
+
+EFL_VERSION = 1.14.2
+EFL_SOURCE = efl-$(EFL_VERSION).tar.xz
+EFL_SITE = http://download.enlightenment.org/rel/libs/efl/
+EFL_LICENSE = BSD-2c, LGPLv2.1+, GPLv2+
+EFL_LICENSE_FILES = COPYING
+
+################################################################################
+#
+# host-efl
+#
+################################################################################
+
+# We want to build only some host tools used later in the build.
+# Actually we want: edje_cc, embryo_cc and eet.
+
+# Host dependencies:
+# * host-dbus: for Eldbus
+# * host-freetype: for libevas
+# * host-libglib2: for libecore
+# * host-libjpeg, host-libpng: for libevas image loader
+# * host-lua: disable luajit dependency
+HOST_EFL_DEPENDENCIES = \
+	host-pkgconf \
+	host-dbus \
+	host-freetype \
+	host-libglib2 \
+	host-libjpeg \
+	host-libpng \
+	host-lua \
+	host-zlib
+
+# Configure options:
+# --disable-audio, --disable-multisense remove libsndfile dependency.
+# --disable-cxx-bindings: disable C++11 bindings.
+# --disable-fontconfig: remove dependency on fontconfig.
+# --disable-fribidi: remove dependency on libfribidi.
+# --disable-gstreamer1: remove dependency on gtreamer 1.0.
+# --disable-libeeze: remove libudev dependency.
+# --disable-libmount: remove dependency on host-util-linux libmount.
+# --disable-physics: remove Bullet dependency.
+# --enable-image-loader-gif=no: disable Gif dependency.
+# --enable-image-loader-tiff=no: disable Tiff dependency.
+# --enable-lua-old: disable Elua and remove luajit dependency.
+# --with-crypto=none: remove dependencies on openssl or gnutls.
+# --with-x11=none: remove dependency on X.org.
+#   Yes I really know what I am doing.
+HOST_EFL_CONF_OPTS += \
+	--disable-audio \
+	--disable-cxx-bindings \
+	--disable-fontconfig \
+	--disable-fribidi \
+	--disable-gstreamer1 \
+	--disable-libeeze \
+	--disable-libmount \
+	--disable-multisense \
+	--disable-physics \
+	--enable-image-loader-gif=no \
+	--enable-image-loader-jpeg=yes \
+	--enable-image-loader-png=yes \
+	--enable-image-loader-tiff=no \
+	--enable-lua-old \
+	--with-crypto=none \
+	--with-glib=yes \
+	--with-opengl=none \
+	--with-x11=none \
+	--enable-i-really-know-what-i-am-doing-and-that-this-will-probably-break-things-and-i-will-fix-them-myself-and-send-patches-aba
+
+$(eval $(host-autotools-package))
