4d7fdf5b17e41649a52bac7fe0050aeb67e775a5
Romain Naour <romain.naour@openwide.fr>
2015-04-18 14:25:38 +0200

    package/efl/expedite: bump to efl-1.14 branch
    Since expedite hasn't been released since 1.7.10 release,
    use efl-1.14 branch in the git tree.
    
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>

--- package/efl/expedite/expedite.mk
+++ package/efl/expedite/expedite.mk
@@ -4,12 +4,17 @@
 #
 ################################################################################
 
-EXPEDITE_VERSION = $(EFL_VERSION)
-EXPEDITE_SITE = http://download.enlightenment.org/releases
+# efl-1.14 branch
+EXPEDITE_VERSION = 9b7d97d5034de22cf8090f5f598eeceab5aa1165
+EXPEDITE_SITE = http://git.enlightenment.org/tools/expedite.git
+EXPEDITE_SITE_METHOD = git
 EXPEDITE_LICENSE = BSD-2c
 EXPEDITE_LICENSE_FILES = COPYING
 
-EXPEDITE_DEPENDENCIES = libevas libeina libeet
+EXPEDITE_DEPENDENCIES = efl-core
+
+# There is no configure script in git tree.
+EXPEDITE_AUTORECONF = YES
 
 ifeq ($(BR2_PACKAGE_LIBEVAS_X11),y)
 EXPEDITE_CONF_OPTS += --with-x=$(STAGING_DIR) \
