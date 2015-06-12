5aeaebf37ca176aa9ae1eb727be3aca8f48774f8
Romain Naour <romain.naour@openwide.fr>
2015-06-12 23:43:31 +0200

    package/elf/libelementary: add host package
    libelementary for the target needs elm_prefs_cc in order to
    cross-compile correctly.
    
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>

--- package/efl/libelementary/libelementary.mk
+++ package/efl/libelementary/libelementary.mk
@@ -12,9 +12,22 @@ LIBELEMENTARY_LICENSE_FILES = COPYING
 
 LIBELEMENTARY_INSTALL_STAGING = YES
 
-LIBELEMENTARY_DEPENDENCIES = host-pkgconf host-efl-core efl-core
+LIBELEMENTARY_DEPENDENCIES = host-pkgconf host-efl-core host-libelementary \
+	efl-core
 
 LIBELEMENTARY_CONF_OPTS = \
+	--with-edje-cc=$(HOST_DIR)/usr/bin/edje_cc \
+	--with-eet-eet=$(HOST_DIR)/usr/bin/eet \
+	--with-eolian-gen=$(HOST_DIR)/usr/bin/eolian_gen \
+	--with-elm-prefs-cc=$(HOST_DIR)/usr/bin/elm_prefs_cc \
+	--with-elementary-codegen=$(HOST_DIR)/usr/bin/elementary_codegen \
+	--with-doxygen=no \
+	--disable-elementary-test
+
+# We need a host package in order to provide elm_prefs_cc and
+# elementary_codegen.
+HOST_LIBELEMENTARY_DEPENDENCIES = host-pkgconf host-efl-core
+HOST_LIBELEMENTARY_CONF_OPTS = \
 	--with-edje-cc=$(HOST_DIR)/usr/bin/edje_cc \
 	--with-eet-eet=$(HOST_DIR)/usr/bin/eet \
 	--with-eolian-gen=$(HOST_DIR)/usr/bin/eolian_gen \
@@ -22,3 +35,4 @@ LIBELEMENTARY_CONF_OPTS = \
 	--disable-elementary-test
 
 $(eval $(autotools-package))
+$(eval $(host-autotools-package))
