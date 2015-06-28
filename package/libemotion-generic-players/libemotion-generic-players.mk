b9a335830df42c8d21b01760b8b44c7075805d2c
Romain Naour <romain.naour@openwide.fr>
2015-06-28 11:58:24 +0200

    package/libemotion-generic-players: new package
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>
    ---
    v6: Add missing vlc dependencies and host-pkgconf (Yann E.Morin)
    v5: Use depends on BR2_PACKAGE_EFL instead of select (ThomasP)
        update comment on dependencies
    v4: update Config.in to use libefl
    v3: remove trailing slash (Vicente Bergas)

---
+++ package/libemotion-generic-players/libemotion-generic-players.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# libemotion-generic-players
+#
+################################################################################
+
+LIBEMOTION_GENERIC_PLAYERS_VERSION = 1.16.0
+LIBEMOTION_GENERIC_PLAYERS_SOURCE = emotion_generic_players-$(LIBEMOTION_GENERIC_PLAYERS_VERSION).tar.xz
+LIBEMOTION_GENERIC_PLAYERS_SITE = http://download.enlightenment.org/rel/libs/emotion_generic_players
+LIBEMOTION_GENERIC_PLAYERS_LICENSE = BSD-2c
+LIBEMOTION_GENERIC_PLAYERS_LICENSE_FILES = COPYING
+
+LIBEMOTION_GENERIC_PLAYERS_INSTALL_STAGING = YES
+
+LIBEMOTION_GENERIC_PLAYERS_DEPENDENCIES = host-pkgconf efl vlc
+
+$(eval $(autotools-package))
