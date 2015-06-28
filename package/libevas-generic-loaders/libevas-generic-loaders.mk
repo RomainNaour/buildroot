92421060a8f5a99d21e19db020440bc61c9857c0
Romain Naour <romain.naour@openwide.fr>
2015-06-28 22:04:49 +0200

    package/libevas-generic-loaders: add libraw dependency
    libraw it's one of the "highly recommended" dependecies
    according to the README [1].
    
    [1] https://git.enlightenment.org/core/evas_generic_loaders.git/tree/README?id=v1.15.0#n31
    
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>
    ---
    v4: improve commit log

--- package/libevas-generic-loaders/libevas-generic-loaders.mk
+++ package/libevas-generic-loaders/libevas-generic-loaders.mk
@@ -14,13 +14,18 @@ LIBEVAS_GENERIC_LOADERS_INSTALL_STAGING = YES
 
 LIBEVAS_GENERIC_LOADERS_DEPENDENCIES = libefl zlib
 
-# For now, we only support the SVG loader
 # poppler >= 0.32 is not supported with libevas-generic-loaders 1.14.0
 LIBEVAS_GENERIC_LOADERS_CONF_OPTS += \
 	--disable-poppler \
 	--disable-spectre \
-	--disable-libraw \
 	--disable-gstreamer
+
+ifeq ($(BR2_PACKAGE_LIBEVAS_GENERIC_LOADERS_LIBRAW),y)
+LIBEVAS_GENERIC_LOADERS_DEPENDENCIES += libraw
+LIBEVAS_GENERIC_LOADERS_CONF_OPTS += --enable-libraw
+else
+LIBEVAS_GENERIC_LOADERS_CONF_OPTS += --disable-libraw
+endif
 
 ifeq ($(BR2_PACKAGE_LIBEVAS_GENERIC_LOADERS_SVG),y)
 LIBEVAS_GENERIC_LOADERS_DEPENDENCIES += librsvg cairo
