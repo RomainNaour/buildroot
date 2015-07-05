498a130a52eddc84a5507a61cdee81dfd1a224ac
Romain Naour <romain.naour@openwide.fr>
2015-07-05 14:13:32 +0200

    package/efl: add sdl2 config option
    SDL support can't be used with OpenGLES.
    
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>

--- package/efl/efl.mk
+++ package/efl/efl.mk
@@ -171,6 +171,17 @@ else
 EFL_CONF_OPTS += --enable-fb=no
 endif
 
+ifeq ($(BR2_PACKAGE_EFL_SDL2),y)
+EFL_CONF_OPTS += --enable-sdl=yes
+EFL_DEPENDENCIES += sdl2
+else
+EFL_CONF_OPTS += --enable-sdl=no
+endif
+
+ifeq ($(BR2_PACKAGE_EFL_X_XLIB_GLX_FULL)$(BR2_PACKAGE_EFL_SDL2),yy)
+EFL_DEPENDENCIES += libglu
+endif
+
 ifeq ($(BR2_PACKAGE_EFL_X),y)
 EFL_CONF_OPTS += --with-x=$(STAGING_DIR) \
 	--x-includes=$(STAGING_DIR)/usr/include \
