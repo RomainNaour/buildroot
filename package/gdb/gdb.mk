ee5fe3763f04cee5305b60b9f1ede19e363c579d
Romain Naour <romain.naour@gmail.com>
2016-06-05 13:49:00 +0200

    autoreconf gdb
    Signed-off-by: Romain Naour <romain.naour@gmail.com>

--- package/gdb/gdb.mk
+++ package/gdb/gdb.mk
@@ -8,6 +8,9 @@ GDB_VERSION = $(call qstrip,$(BR2_GDB_VERSION))
 GDB_SITE = $(BR2_GNU_MIRROR)/gdb
 GDB_SOURCE = gdb-$(GDB_VERSION).tar.xz
 
+GDB_LIBTOOL_PATCH = NO
+GDB_AUTORECONF = YES
+
 ifeq ($(BR2_arc),y)
 GDB_SITE = $(call github,foss-for-synopsys-dwc-arc-processors,binutils-gdb,$(GDB_VERSION))
 GDB_SOURCE = gdb-$(GDB_VERSION).tar.gz
