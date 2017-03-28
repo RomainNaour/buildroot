60d4ddaada8b351a3aa4bebcae2f30a35c9336e0
Romain Naour <romain.naour@gmail.com>
2017-03-28 21:51:22 +0200

    package/mesa3d: enable llvm support
    Signed-off-by: Romain Naour <romain.naour@gmail.com>
    Cc: Adrian Perez de Castro <aperez@igalia.com>

--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -26,6 +26,20 @@ MESA3D_DEPENDENCIES = \
 # Disable assembly usage.
 MESA3D_CONF_OPTS = --disable-asm
 
+ifeq ($(BR2_PACKAGE_MESA3D_LLVM),y)
+MESA3D_DEPENDENCIES += host-llvm llvm
+MESA3D_CONF_OPTS += \
+	--with-llvm-prefix=$(STAGING_DIR)/usr \
+	--enable-llvm-shared-libs \
+	--enable-llvm
+else
+# Avoid automatic search of llvm-config
+MESA3D_CONF_OPTS += \
+	--with-llvm-prefix=$(STAGING_DIR)/usr \
+	--disable-llvm-shared-libs \
+	--disable-llvm
+endif
+
 # The Sourcery MIPS toolchain has a special (non-upstream) feature to
 # have "compact exception handling", which unfortunately breaks with
 # mesa3d, so we disable it here by passing -mno-compact-eh.
@@ -212,8 +226,5 @@ MESA3D_DEPENDENCIES += lm-sensors
 else
 MESA3D_CONF_OPTS += --disable-lmsensors
 endif
-
-# Avoid automatic search of llvm-config
-MESA3D_CONF_OPTS += --with-llvm-prefix=$(STAGING_DIR)/usr/bin
 
 $(eval $(autotools-package))
