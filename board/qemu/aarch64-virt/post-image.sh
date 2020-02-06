5ace4d8733c143edb0e33a46000e3e51d9909bd1
Romain Naour <romain.naour@smile.fr>
2020-02-06 23:56:24 +0100

    add gitlab handling
    Signed-off-by: Romain Naour <romain.naour@smile.fr>

--- board/qemu/aarch64-virt/post-image.sh
+++ board/qemu/aarch64-virt/post-image.sh
@@ -8,6 +8,13 @@ if [ -f $README_FILE ]; then
     DEFCONFIG_NAME=$(cat $README_FILE | sed -r '/^.*: /!d; :a; /\\$/N; ta; s/:.*$//')
     QEMU_CMD_LINE=$(cat $README_FILE | sed -r "/^${DEFCONFIG_NAME}: /!d; :a; /\\$/N; s/\\\n//; ta; s/^[^:]+://")
     QEMU_CMD_LINE=${QEMU_CMD_LINE//output\/images/\${IMAGE_DIR\}}
+    # Test if we are running in gitlab
+    if [ -n "$CI_JOB_NAME" ]; then
+        # Remove -serial stdio if present
+        QEMU_CMD_LINE=${QEMU_CMD_LINE//-serial stdio/}
+        # Disable graphical output and redirect serial I/Os to console
+        QEMU_CMD_LINE="$QEMU_CMD_LINE -nographic"
+    fi
     cat << EOF > $START_QEMU_SCRIPT
 #!/bin/sh
 IMAGE_DIR="\$(dirname \$0)"
