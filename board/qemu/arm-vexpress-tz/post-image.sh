09c746189e6455c2aff4eb8fb4d9bd42dd2f91fd
Romain Naour <romain.naour@smile.fr>
2020-02-08 15:20:28 +0100

    add post-image script for qemu testing
    Signed-off-by: Romain Naour <romain.naour@smile.fr>

---
+++ board/qemu/arm-vexpress-tz/post-image.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+BOARD_DIR="$(dirname $0)"
+DEFCONFIG_NAME="$(basename $2)"
+README_FILE="${BOARD_DIR}/readme.txt"
+START_QEMU_SCRIPT="${BINARIES_DIR}/start-qemu.sh"
+
+# if [[ "${f}" =~ ^"${ignore}" ]]; then
+if [[ "${DEFCONFIG_NAME}" =~ ^"qemu_*" ]]; then
+    # Not a Qemu defconfig, can't test.
+    return
+fi
+
+if [ -f $README_FILE ]; then
+    QEMU_CMD_LINE=$(cat $README_FILE | sed -r "/^${DEFCONFIG_NAME}: /!d; :a; /\\$/N; s/\\\n//; ta; s/^[^:]+://")
+    QEMU_CMD_LINE=${QEMU_CMD_LINE//output\/images/\${IMAGE_DIR\}}
+    # Test if we are running in gitlab
+    if [ -n "$CI_JOB_NAME" ]; then
+        # Remove -serial stdio if present
+        QEMU_CMD_LINE=${QEMU_CMD_LINE//-serial stdio/}
+        # Disable graphical output and redirect serial I/Os to console
+        QEMU_CMD_LINE="$QEMU_CMD_LINE -nographic"
+    fi
+    cat << EOF > $START_QEMU_SCRIPT
+#!/bin/sh
+IMAGE_DIR="\$(dirname \$0)"
+
+$QEMU_CMD_LINE
+EOF
+    chmod +x $START_QEMU_SCRIPT
+fi
