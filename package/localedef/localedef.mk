20819061b2f6c7f1ae31d22b7fa869ad1810b2e4
Romain Naour <romain.naour@gmail.com>
2021-02-20 17:41:35 +0100

    package/glibc: bump to version 2.33-6-g8d4241b8976273513e72cc1c5f6b1af3e11f0792
    Signed-off-by: Romain Naour <romain.naour@gmail.com>

--- package/localedef/localedef.mk
+++ package/localedef/localedef.mk
@@ -7,7 +7,7 @@
 # Use the same VERSION and SITE as target glibc
 # As in glibc.mk, generate version string using:
 #   git describe --match 'glibc-*' --abbrev=40 origin/release/MAJOR.MINOR/master | cut -d '-' -f 2-
-LOCALEDEF_VERSION = 2.32-37-g760e1d287825fa91d4d5a0cc921340c740d803e2
+LOCALEDEF_VERSION = 2.33-6-g8d4241b8976273513e72cc1c5f6b1af3e11f0792
 LOCALEDEF_SOURCE = glibc-$(LOCALEDEF_VERSION).tar.gz
 LOCALEDEF_SITE = $(call github,bminor,glibc,$(LOCALEDEF_VERSION))
 HOST_LOCALEDEF_DL_SUBDIR = glibc
